9bebaf7513b06adfa91689e3ec95a2a301a4b9fd
Hristo Oskov <hristo.oskov@gmail.com>
2017-12-14 11:19:09 -0800

    testing index.js for both default export and named exports; add tests to catch forgotten exports; use fs.readdirSync

---
+++ src/index_named_exports.test.js
@@ -0,0 +1,33 @@
+import fs from 'fs';
+import path from 'path';
+
+import PACKAGE from '../package.json';
+
+import { OBJECT_TAG } from './utils/TestUtils';
+
+import * as LatticeSagas from './index';
+
+const SAGAS_DIR = path.join(__dirname, 'sagas');
+
+describe('lattice-sagas named exports', () => {
+
+  test('should export the correct version', () => {
+    expect(LatticeSagas.version).toEqual(PACKAGE.version);
+  });
+
+  fs.readdirSync(SAGAS_DIR)
+    .filter((fileName) => {
+      // exclude files that end with ".test.js"
+      return !(/\.test\.js$/.test(fileName));
+    })
+    .forEach((fileName) => {
+      const expectedExport = fileName.slice(0, -3); // strip the ".js"
+      test(`should export ${expectedExport}`, () => {
+        expect(LatticeSagas).toHaveProperty(expectedExport);
+        expect(Object.prototype.toString.call(LatticeSagas[expectedExport])).toEqual(OBJECT_TAG);
+        expect(Object.keys(LatticeSagas[expectedExport]).length).toBeGreaterThan(0);
+        expect(Object.keys(LatticeSagas[expectedExport]).length % 2).toEqual(0);
+      });
+    });
+
+});
