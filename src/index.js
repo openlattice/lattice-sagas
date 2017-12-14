b2227df2784d8998494514032b110283aca2d691
Hristo Oskov <hristo.oskov@gmail.com>
2017-12-13 20:22:45 -0800

    forgot to export

--- src/index.js
+++ src/index.js
@@ -4,10 +4,14 @@
 
 import * as AppApiActionFactory from './sagas/AppApiActionFactory';
 import * as AppApiSagas from './sagas/AppApiSagas';
+import * as DataApiActionFactory from './sagas/DataApiActionFactory';
+import * as DataApiSagas from './sagas/DataApiSagas';
 import * as EntityDataModelApiActionFactory from './sagas/EntityDataModelApiActionFactory';
 import * as EntityDataModelApiSagas from './sagas/EntityDataModelApiSagas';
 import * as SearchApiActionFactory from './sagas/SearchApiActionFactory';
 import * as SearchApiSagas from './sagas/SearchApiSagas';
+import * as SyncApiActionFactory from './sagas/SyncApiActionFactory';
+import * as SyncApiSagas from './sagas/SyncApiSagas';
 
 // injected by Webpack.DefinePlugin
 declare var __VERSION__ :string;
@@ -16,19 +20,27 @@ const version :string = __VERSION__;
 export {
   AppApiActionFactory,
   AppApiSagas,
+  DataApiActionFactory,
+  DataApiSagas,
   EntityDataModelApiActionFactory,
   EntityDataModelApiSagas,
   SearchApiActionFactory,
   SearchApiSagas,
+  SyncApiActionFactory,
+  SyncApiSagas,
   version
 };
 
 export default {
   AppApiActionFactory,
   AppApiSagas,
+  DataApiActionFactory,
+  DataApiSagas,
   EntityDataModelApiActionFactory,
   EntityDataModelApiSagas,
   SearchApiActionFactory,
   SearchApiSagas,
+  SyncApiActionFactory,
+  SyncApiSagas,
   version
 };
