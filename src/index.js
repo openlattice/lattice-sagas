ca013dbf7447ddac338a9b57d2dfe8f482f47cb6
Hristo Oskov <hristo.oskov@gmail.com>
2018-07-18 20:24:00 -0700

    adding DataIntegrationApi, upgrading DataApi, deleting SyncApi

--- src/index.js
+++ src/index.js
@@ -6,12 +6,12 @@ import * as AppApiActionFactory from './sagas/AppApiActionFactory';
 import * as AppApiSagas from './sagas/AppApiSagas';
 import * as DataApiActionFactory from './sagas/DataApiActionFactory';
 import * as DataApiSagas from './sagas/DataApiSagas';
+import * as DataIntegrationApiActionFactory from './sagas/DataIntegrationApiActionFactory';
+import * as DataIntegrationApiSagas from './sagas/DataIntegrationApiSagas';
 import * as EntityDataModelApiActionFactory from './sagas/EntityDataModelApiActionFactory';
 import * as EntityDataModelApiSagas from './sagas/EntityDataModelApiSagas';
 import * as SearchApiActionFactory from './sagas/SearchApiActionFactory';
 import * as SearchApiSagas from './sagas/SearchApiSagas';
-import * as SyncApiActionFactory from './sagas/SyncApiActionFactory';
-import * as SyncApiSagas from './sagas/SyncApiSagas';
 
 // injected by Webpack.DefinePlugin
 declare var __VERSION__ :string;
@@ -22,12 +22,12 @@ export {
   AppApiSagas,
   DataApiActionFactory,
   DataApiSagas,
+  DataIntegrationApiActionFactory,
+  DataIntegrationApiSagas,
   EntityDataModelApiActionFactory,
   EntityDataModelApiSagas,
   SearchApiActionFactory,
   SearchApiSagas,
-  SyncApiActionFactory,
-  SyncApiSagas,
   version
 };
 
@@ -36,11 +36,11 @@ export default {
   AppApiSagas,
   DataApiActionFactory,
   DataApiSagas,
+  DataIntegrationApiActionFactory,
+  DataIntegrationApiSagas,
   EntityDataModelApiActionFactory,
   EntityDataModelApiSagas,
   SearchApiActionFactory,
   SearchApiSagas,
-  SyncApiActionFactory,
-  SyncApiSagas,
   version
 };
--- src/sagas/DataApiActionFactory.js
+++ src/sagas/DataApiActionFactory.js
@@ -4,20 +4,10 @@
 
 import { newRequestSequence } from 'redux-reqseq';
 
-const ACQUIRE_SYNC_TICKET :'ACQUIRE_SYNC_TICKET' = 'ACQUIRE_SYNC_TICKET';
-const acquireSyncTicket :RequestSequence = newRequestSequence(ACQUIRE_SYNC_TICKET);
-
-const CREATE_ENTITY_AND_ASSOCIATION_DATA :'CREATE_ENTITY_AND_ASSOCIATION_DATA' = 'CREATE_ENTITY_AND_ASSOCIATION_DATA';
-const createEntityAndAssociationData :RequestSequence = newRequestSequence(CREATE_ENTITY_AND_ASSOCIATION_DATA);
-
 const GET_ENTITY_SET_DATA :'GET_ENTITY_SET_DATA' = 'GET_ENTITY_SET_DATA';
 const getEntitySetData :RequestSequence = newRequestSequence(GET_ENTITY_SET_DATA);
 
 export {
-  ACQUIRE_SYNC_TICKET,
-  CREATE_ENTITY_AND_ASSOCIATION_DATA,
   GET_ENTITY_SET_DATA,
-  acquireSyncTicket,
-  createEntityAndAssociationData,
   getEntitySetData
 };
