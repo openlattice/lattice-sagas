d6d4a23f259e7aa656c5e4d4b12bb5295ce2805a
Hristo Oskov <hristo.oskov@gmail.com>
2018-10-01 11:15:39 -0700

    DataApi.clearEntityFromEntitySet

--- src/sagas/DataApiActions.js
+++ src/sagas/DataApiActions.js
@@ -4,6 +4,9 @@
 
 import { newRequestSequence } from 'redux-reqseq';
 
+const CLEAR_ENTITY_FROM_ENTITY_SET :'CLEAR_ENTITY_FROM_ENTITY_SET' = 'CLEAR_ENTITY_FROM_ENTITY_SET';
+const clearEntityFromEntitySet :RequestSequence = newRequestSequence(CLEAR_ENTITY_FROM_ENTITY_SET);
+
 const GET_ENTITY_DATA :'GET_ENTITY_DATA' = 'GET_ENTITY_DATA';
 const getEntityData :RequestSequence = newRequestSequence(GET_ENTITY_DATA);
 
@@ -14,9 +17,11 @@ const UPDATE_ENTITY_DATA :'UPDATE_ENTITY_DATA' = 'UPDATE_ENTITY_DATA';
 const updateEntityData :RequestSequence = newRequestSequence(UPDATE_ENTITY_DATA);
 
 export {
+  CLEAR_ENTITY_FROM_ENTITY_SET,
   GET_ENTITY_DATA,
   GET_ENTITY_SET_DATA,
   UPDATE_ENTITY_DATA,
+  clearEntityFromEntitySet,
   getEntityData,
   getEntitySetData,
   updateEntityData,
--- src/sagas/DataApiActions.test.js
+++ src/sagas/DataApiActions.test.js
@@ -4,12 +4,14 @@ import * as DataApiActions from './DataApiActions';
 import { testShouldExportActionTypes, testShouldExportRequestSequences } from '../utils/testing/TestUtils';
 
 const ACTION_TYPES = Set([
+  'CLEAR_ENTITY_FROM_ENTITY_SET',
   'GET_ENTITY_DATA',
   'GET_ENTITY_SET_DATA',
   'UPDATE_ENTITY_DATA',
 ]).sort();
 
 const REQSEQ_NAMES = Set([
+  'clearEntityFromEntitySet',
   'getEntityData',
   'getEntitySetData',
   'updateEntityData',
