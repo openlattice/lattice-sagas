ae6efef7e9ce127c2c3f5be2be68724750959035
Hristo Oskov <hristo@openlattice.com>
2019-08-07 17:52:29 -0700

    getOrganizationIntegrationAccount ; renaming a bunch of actions

--- src/sagas/OrganizationsApiActions.js
+++ src/sagas/OrganizationsApiActions.js
@@ -5,11 +5,11 @@
 import { newRequestSequence } from 'redux-reqseq';
 import type { RequestSequence } from 'redux-reqseq';
 
-const ADD_AUTO_APPROVED_DOMAIN :'ADD_AUTO_APPROVED_DOMAIN' = 'ADD_AUTO_APPROVED_DOMAIN';
-const addAutoApprovedDomain :RequestSequence = newRequestSequence(ADD_AUTO_APPROVED_DOMAIN);
+const ADD_DOMAIN_TO_ORG :'ADD_DOMAIN_TO_ORG' = 'ADD_DOMAIN_TO_ORG';
+const addDomainToOrganization :RequestSequence = newRequestSequence(ADD_DOMAIN_TO_ORG);
 
-const ADD_MEMBER_TO_ORGANIZATION :'ADD_MEMBER_TO_ORGANIZATION' = 'ADD_MEMBER_TO_ORGANIZATION';
-const addMemberToOrganization :RequestSequence = newRequestSequence(ADD_MEMBER_TO_ORGANIZATION);
+const ADD_MEMBER_TO_ORG :'ADD_MEMBER_TO_ORG' = 'ADD_MEMBER_TO_ORG';
+const addMemberToOrganization :RequestSequence = newRequestSequence(ADD_MEMBER_TO_ORG);
 
 const CREATE_ROLE :'CREATE_ROLE' = 'CREATE_ROLE';
 const createRole :RequestSequence = newRequestSequence(CREATE_ROLE);
@@ -26,34 +26,39 @@ const getAllOrganizations :RequestSequence = newRequestSequence(GET_ALL_ORGANIZA
 const GET_ORGANIZATION :'GET_ORGANIZATION' = 'GET_ORGANIZATION';
 const getOrganization :RequestSequence = newRequestSequence(GET_ORGANIZATION);
 
-const GET_ORGANIZATION_MEMBERS :'GET_ORGANIZATION_MEMBERS' = 'GET_ORGANIZATION_MEMBERS';
-const getOrganizationMembers :RequestSequence = newRequestSequence(GET_ORGANIZATION_MEMBERS);
+const GET_ORG_INTEGRATION_ACCOUNT :'GET_ORG_INTEGRATION_ACCOUNT' = 'GET_ORG_INTEGRATION_ACCOUNT';
+const getOrganizationIntegrationAccount :RequestSequence = newRequestSequence(GET_ORG_INTEGRATION_ACCOUNT);
 
-const REMOVE_AUTO_APPROVED_DOMAIN :'REMOVE_AUTO_APPROVED_DOMAIN' = 'REMOVE_AUTO_APPROVED_DOMAIN';
-const removeAutoApprovedDomain :RequestSequence = newRequestSequence(REMOVE_AUTO_APPROVED_DOMAIN);
+const GET_ORG_MEMBERS :'GET_ORG_MEMBERS' = 'GET_ORG_MEMBERS';
+const getOrganizationMembers :RequestSequence = newRequestSequence(GET_ORG_MEMBERS);
 
-const REMOVE_MEMBER_FROM_ORGANIZATION :'REMOVE_MEMBER_FROM_ORGANIZATION' = 'REMOVE_MEMBER_FROM_ORGANIZATION';
-const removeMemberFromOrganization :RequestSequence = newRequestSequence(REMOVE_MEMBER_FROM_ORGANIZATION);
+const REMOVE_DOMAIN_FROM_ORG :'REMOVE_DOMAIN_FROM_ORG' = 'REMOVE_DOMAIN_FROM_ORG';
+const removeDomainFromOrganization :RequestSequence = newRequestSequence(REMOVE_DOMAIN_FROM_ORG);
+
+const REMOVE_MEMBER_FROM_ORG :'REMOVE_MEMBER_FROM_ORG' = 'REMOVE_MEMBER_FROM_ORG';
+const removeMemberFromOrganization :RequestSequence = newRequestSequence(REMOVE_MEMBER_FROM_ORG);
 
 export {
-  ADD_AUTO_APPROVED_DOMAIN,
-  ADD_MEMBER_TO_ORGANIZATION,
+  ADD_DOMAIN_TO_ORG,
+  ADD_MEMBER_TO_ORG,
   CREATE_ROLE,
   DELETE_ORGANIZATION,
   DELETE_ROLE,
   GET_ALL_ORGANIZATIONS,
   GET_ORGANIZATION,
-  GET_ORGANIZATION_MEMBERS,
-  REMOVE_AUTO_APPROVED_DOMAIN,
-  REMOVE_MEMBER_FROM_ORGANIZATION,
-  addAutoApprovedDomain,
+  GET_ORG_INTEGRATION_ACCOUNT,
+  GET_ORG_MEMBERS,
+  REMOVE_DOMAIN_FROM_ORG,
+  REMOVE_MEMBER_FROM_ORG,
+  addDomainToOrganization,
   addMemberToOrganization,
   createRole,
   deleteOrganization,
   deleteRole,
   getAllOrganizations,
   getOrganization,
+  getOrganizationIntegrationAccount,
   getOrganizationMembers,
-  removeAutoApprovedDomain,
+  removeDomainFromOrganization,
   removeMemberFromOrganization,
 };
