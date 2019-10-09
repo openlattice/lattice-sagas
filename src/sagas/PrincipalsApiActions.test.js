a6de814f072fd624fa034bb4414a0bf28c374c2e
Hristo Oskov <hristo@openlattice.com>
2019-10-08 18:57:58 -0700

    PrincipalsApi.getAllUsers

--- src/sagas/PrincipalsApiActions.test.js
+++ src/sagas/PrincipalsApiActions.test.js
@@ -8,12 +8,14 @@ import * as PrincipalsApiActions from './PrincipalsApiActions';
 import { testShouldExportActionTypes, testShouldExportRequestSequences } from '../utils/testing/TestUtils';
 
 const ACTION_TYPES = Immutable.List([
+  'GET_ALL_USERS',
   'GET_CURRENT_ROLES',
   'GET_SECURABLE_PRINCIPAL',
   'SEARCH_ALL_USERS',
 ]).sort();
 
 const REQSEQ_NAMES = Immutable.List([
+  'getAllUsers',
   'getCurrentRoles',
   'getSecurablePrincipal',
   'searchAllUsers',
