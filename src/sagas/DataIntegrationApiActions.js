c34bab4e93422af3cbebab52648b6c3b900f8fd0
Hristo Oskov <hristo@openlattice.com>
2020-04-28 23:22:19 -0700

    DataIntegrationApi

--- src/sagas/DataIntegrationApiActions.js
+++ src/sagas/DataIntegrationApiActions.js
@@ -5,15 +5,10 @@
 import { newRequestSequence } from 'redux-reqseq';
 import type { RequestSequence } from 'redux-reqseq';
 
-const CREATE_ENTITY_AND_ASSOCIATION_DATA :'CREATE_ENTITY_AND_ASSOCIATION_DATA' = 'CREATE_ENTITY_AND_ASSOCIATION_DATA';
-const createEntityAndAssociationData :RequestSequence = newRequestSequence(CREATE_ENTITY_AND_ASSOCIATION_DATA);
-
 const GET_ENTITY_KEY_IDS :'GET_ENTITY_KEY_IDS' = 'GET_ENTITY_KEY_IDS';
 const getEntityKeyIds :RequestSequence = newRequestSequence(GET_ENTITY_KEY_IDS);
 
 export {
-  CREATE_ENTITY_AND_ASSOCIATION_DATA,
   GET_ENTITY_KEY_IDS,
-  createEntityAndAssociationData,
   getEntityKeyIds,
 };
--- src/sagas/DataIntegrationApiSagas.test.js
+++ src/sagas/DataIntegrationApiSagas.test.js
@@ -6,15 +6,11 @@ import { v4 as uuid } from 'uuid';
 import { DataIntegrationApi } from 'lattice';
 
 import {
-  CREATE_ENTITY_AND_ASSOCIATION_DATA,
   GET_ENTITY_KEY_IDS,
-  createEntityAndAssociationData,
   getEntityKeyIds,
 } from './DataIntegrationApiActions';
 
 import {
-  createEntityAndAssociationDataWatcher,
-  createEntityAndAssociationDataWorker,
   getEntityKeyIdsWatcher,
   getEntityKeyIdsWorker,
 } from './DataIntegrationApiSagas';
@@ -31,49 +27,8 @@ describe('DataIntegrationApiSagas', () => {
 
   /*
    *
-   * DataIntegrationApi.createEntityAndAssociationData
-   * DataIntegrationApiActions.createEntityAndAssociationData
-   *
-   */
-
-  describe('createEntityAndAssociationDataWatcher', () => {
-    testShouldBeGeneratorFunction(createEntityAndAssociationDataWatcher);
-    testWatcherSagaShouldTakeEvery(
-      createEntityAndAssociationDataWatcher,
-      createEntityAndAssociationDataWorker,
-      CREATE_ENTITY_AND_ASSOCIATION_DATA
-    );
-  });
-
-  describe('createEntityAndAssociationDataWorker', () => {
-
-    const mockActionValue = uuid();
-
-    testShouldBeGeneratorFunction(createEntityAndAssociationDataWorker);
-    testShouldFailOnInvalidAction(createEntityAndAssociationDataWorker, CREATE_ENTITY_AND_ASSOCIATION_DATA);
-
-    testWorkerSagaShouldHandleSuccessCase({
-      latticeApi: DataIntegrationApi.createEntityAndAssociationData,
-      latticeApiParams: [mockActionValue],
-      latticeApiReqSeq: createEntityAndAssociationData,
-      workerSagaAction: createEntityAndAssociationData(mockActionValue),
-      workerSagaToTest: createEntityAndAssociationDataWorker
-    });
-
-    testWorkerSagaShouldHandleFailureCase({
-      latticeApi: DataIntegrationApi.createEntityAndAssociationData,
-      latticeApiParams: [mockActionValue],
-      latticeApiReqSeq: createEntityAndAssociationData,
-      workerSagaAction: createEntityAndAssociationData(mockActionValue),
-      workerSagaToTest: createEntityAndAssociationDataWorker
-    });
-
-  });
-
-  /*
-   *
-   * DataIntegrationApi.createEntityAndAssociationData
-   * DataIntegrationApiActions.createEntityAndAssociationData
+   * DataIntegrationApi.getEntityKeyIds
+   * DataIntegrationApiActions.getEntityKeyIds
    *
    */
 
@@ -82,16 +37,13 @@ describe('DataIntegrationApiSagas', () => {
     testWatcherSagaShouldTakeEvery(
       getEntityKeyIdsWatcher,
       getEntityKeyIdsWorker,
-      GET_ENTITY_KEY_IDS
+      GET_ENTITY_KEY_IDS,
     );
   });
 
   describe('getEntityKeyIdsWorker', () => {
 
-    const mockActionValue = {
-      entitySetId: uuid(),
-      entityId: uuid()
-    };
+    const mockActionValue = [uuid(), uuid()];
 
     testShouldBeGeneratorFunction(getEntityKeyIdsWorker);
     testShouldFailOnInvalidAction(getEntityKeyIdsWorker, GET_ENTITY_KEY_IDS);
@@ -101,7 +53,7 @@ describe('DataIntegrationApiSagas', () => {
       latticeApiParams: [mockActionValue],
       latticeApiReqSeq: getEntityKeyIds,
       workerSagaAction: getEntityKeyIds(mockActionValue),
-      workerSagaToTest: getEntityKeyIdsWorker
+      workerSagaToTest: getEntityKeyIdsWorker,
     });
 
     testWorkerSagaShouldHandleFailureCase({
@@ -109,9 +61,8 @@ describe('DataIntegrationApiSagas', () => {
       latticeApiParams: [mockActionValue],
       latticeApiReqSeq: getEntityKeyIds,
       workerSagaAction: getEntityKeyIds(mockActionValue),
-      workerSagaToTest: getEntityKeyIdsWorker
+      workerSagaToTest: getEntityKeyIdsWorker,
     });
-
   });
 
 });
