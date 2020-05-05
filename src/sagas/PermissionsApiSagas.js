c79a82ede49f4a17c6c1f93e569aa746ec7ba3d1
Hristo Oskov <hristo@openlattice.com>
2020-05-04 18:21:46 -0700

    PermissionsApi

--- src/sagas/PermissionsApiSagas.js
+++ src/sagas/PermissionsApiSagas.js
@@ -4,156 +4,179 @@
 
 import { call, put, takeEvery } from '@redux-saga/core/effects';
 import { PermissionsApi } from 'lattice';
+import type { Saga } from '@redux-saga/core';
 import type { SequenceAction } from 'redux-reqseq';
-
-import { ERR_INVALID_ACTION, ERR_ACTION_VALUE_NOT_DEFINED } from '../utils/Errors';
-import { isValidAction } from '../utils/Utils';
 
 import {
   GET_ACL,
+  GET_ACL_EXPLANATION,
   UPDATE_ACL,
   UPDATE_ACLS,
   getAcl,
+  getAclExplanation,
   updateAcl,
   updateAcls,
 } from './PermissionsApiActions';
 
+import { ERR_INVALID_ACTION } from '../utils/Errors';
+import { isValidAction } from '../utils/Utils';
+import type { WorkerResponse } from '../types';
+
 /*
  *
- * PermissionsApi.getAcl()
- * PermissionsApiActions.getAcl()
+ * PermissionsApi.getAcl
+ * PermissionsApiActions.getAcl
  *
  */
 
-function* getAclWorker(seqAction :SequenceAction) :Generator<*, *, *> {
+function* getAclWorker(action :SequenceAction) :Saga<WorkerResponse> {
 
-  if (!isValidAction(seqAction, GET_ACL)) {
-    return {
-      error: ERR_INVALID_ACTION
-    };
+  if (!isValidAction(action, GET_ACL)) {
+    return { error: new Error(ERR_INVALID_ACTION) };
   }
 
-  const { id, value } = seqAction;
-  if (value === null || value === undefined) {
-    return {
-      error: ERR_ACTION_VALUE_NOT_DEFINED
-    };
-  }
-
-  const response :Object = {};
+  let workerResponse :WorkerResponse;
+  const { id, value } = action;
 
   try {
     yield put(getAcl.request(id, value));
-    response.data = yield call(PermissionsApi.getAcl, value);
-    yield put(getAcl.success(id, response.data));
+    const response = yield call(PermissionsApi.getAcl, value);
+    workerResponse = { data: response };
+    yield put(getAcl.success(id, response));
   }
   catch (error) {
-    response.error = error;
-    yield put(getAcl.failure(id, response.error));
+    workerResponse = { error };
+    yield put(getAcl.failure(id, error));
   }
   finally {
     yield put(getAcl.finally(id));
   }
 
-  return response;
+  return workerResponse;
 }
 
-function* getAclWatcher() :Generator<*, void, *> {
+function* getAclWatcher() :Saga<*> {
 
   yield takeEvery(GET_ACL, getAclWorker);
 }
 
 /*
  *
- * PermissionsApi.updateAcl()
- * PermissionsApiActions.updateAcl()
+ * PermissionsApi.getAclExplanation
+ * PermissionsApiActions.getAclExplanation
  *
  */
 
-function* updateAclWorker(seqAction :SequenceAction) :Generator<*, *, *> {
+function* getAclExplanationWorker(action :SequenceAction) :Saga<WorkerResponse> {
 
-  if (!isValidAction(seqAction, UPDATE_ACL)) {
-    return {
-      error: ERR_INVALID_ACTION
-    };
+  if (!isValidAction(action, GET_ACL_EXPLANATION)) {
+    return { error: new Error(ERR_INVALID_ACTION) };
   }
 
-  const { id, value } = seqAction;
-  if (value === null || value === undefined) {
-    return {
-      error: ERR_ACTION_VALUE_NOT_DEFINED
-    };
+  let workerResponse :WorkerResponse;
+  const { id, value } = action;
+
+  try {
+    yield put(getAclExplanation.request(id, value));
+    const response = yield call(PermissionsApi.getAclExplanation, value);
+    workerResponse = { data: response };
+    yield put(getAclExplanation.success(id, response));
+  }
+  catch (error) {
+    workerResponse = { error };
+    yield put(getAclExplanation.failure(id, error));
+  }
+  finally {
+    yield put(getAclExplanation.finally(id));
   }
 
-  const response :Object = {};
+  return workerResponse;
+}
+
+function* getAclExplanationWatcher() :Saga<*> {
+
+  yield takeEvery(GET_ACL_EXPLANATION, getAclExplanationWorker);
+}
+
+/*
+ *
+ * PermissionsApi.updateAcl
+ * PermissionsApiActions.updateAcl
+ *
+ */
+
+function* updateAclWorker(action :SequenceAction) :Saga<WorkerResponse> {
+
+  if (!isValidAction(action, UPDATE_ACL)) {
+    return { error: new Error(ERR_INVALID_ACTION) };
+  }
+
+  let workerResponse :WorkerResponse;
+  const { id, value } = action;
 
   try {
     yield put(updateAcl.request(id, value));
-    response.data = yield call(PermissionsApi.updateAcl, value);
-    yield put(updateAcl.success(id, response.data));
+    const response = yield call(PermissionsApi.updateAcl, value);
+    workerResponse = { data: response };
+    yield put(updateAcl.success(id, response));
   }
   catch (error) {
-    response.error = error;
-    yield put(updateAcl.failure(id, response.error));
+    workerResponse = { error };
+    yield put(updateAcl.failure(id, error));
   }
   finally {
     yield put(updateAcl.finally(id));
   }
 
-  return response;
+  return workerResponse;
 }
 
-function* updateAclWatcher() :Generator<*, void, *> {
+function* updateAclWatcher() :Saga<*> {
 
   yield takeEvery(UPDATE_ACL, updateAclWorker);
 }
 
 /*
  *
- * PermissionsApi.updateAcls()
- * PermissionsApiActions.updateAcls()
+ * PermissionsApi.updateAcls
+ * PermissionsApiActions.updateAcls
  *
  */
 
-function* updateAclsWorker(seqAction :SequenceAction) :Generator<*, *, *> {
+function* updateAclsWorker(action :SequenceAction) :Saga<WorkerResponse> {
 
-  if (!isValidAction(seqAction, UPDATE_ACLS)) {
-    return {
-      error: ERR_INVALID_ACTION
-    };
+  if (!isValidAction(action, UPDATE_ACLS)) {
+    return { error: new Error(ERR_INVALID_ACTION) };
   }
 
-  const { id, value } = seqAction;
-  if (value === null || value === undefined) {
-    return {
-      error: ERR_ACTION_VALUE_NOT_DEFINED
-    };
-  }
-
-  const response :Object = {};
+  let workerResponse :WorkerResponse;
+  const { id, value } = action;
 
   try {
     yield put(updateAcls.request(id, value));
-    response.data = yield call(PermissionsApi.updateAcls, value);
-    yield put(updateAcls.success(id, response.data));
+    const response = yield call(PermissionsApi.updateAcls, value);
+    workerResponse = { data: response };
+    yield put(updateAcls.success(id, response));
   }
   catch (error) {
-    response.error = error;
-    yield put(updateAcls.failure(id, response.error));
+    workerResponse = { error };
+    yield put(updateAcls.failure(id, error));
   }
   finally {
     yield put(updateAcls.finally(id));
   }
 
-  return response;
+  return workerResponse;
 }
 
-function* updateAclsWatcher() :Generator<*, void, *> {
+function* updateAclsWatcher() :Saga<*> {
 
   yield takeEvery(UPDATE_ACLS, updateAclsWorker);
 }
 
 export {
+  getAclExplanationWatcher,
+  getAclExplanationWorker,
   getAclWatcher,
   getAclWorker,
   updateAclWatcher,
